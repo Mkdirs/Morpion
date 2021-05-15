b8a3c690046ec4a56ac953efbf18df82b4dbaef9
Mkdirs <976kiritou@gmail.com>
2021-05-15 14:10:20 +0200

    -Ajout d'un interpréteur python embarqué -Ajout du module pygame -Début d'un écran de démarrage
    PS: C'est mort je ne vais pas faire l'interface graphique tout seul.

--- Morpion.py
+++ Morpion.py
@@ -1,4 +1,6 @@
 from Controleur import *
+import pygame
+
 
 def jouer():
     controleur = Controleur()
@@ -32,4 +34,19 @@ def jouer():
 
 
 if __name__ == '__main__':
-    jouer()
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 720))
+    pygame.display.set_caption("Morpion")
+    font = pygame.font.SysFont(pygame.font.get_default_font(), 24)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+        txt = font.render("Hello World", True, [0]*3)
+        screen.blit(txt, (500-txt.get_width()/2, 720/2-txt.get_height()/2))
+        pygame.display.update()
+
+    pygame.quit()
